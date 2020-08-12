9a71c22a7cc77fa7151b3b0a3eea769809a87e1b
James LaRocque <james.larocque@gmail.com>
2020-08-11 22:23:39 -0600

    Base BDD with .NET Core & SpecFlow
    This is from
    https://www.testautomation.org/setting-up-specflow-on-a-dotnet-core-project-using-visual-studio-code-with-linux-os

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace bdd
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
